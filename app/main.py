0e0001f3bf407accc788e73f8e08f666d6bca576
regfe89 <marat.ismailov889@gmail.com>
2021-04-22 13:31:02 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -41,7 +41,55 @@ async def index(request: Request):
     # return render_template('report.html', hists = hists)
     return templates.TemplateResponse("index.html", {"request": request, "plots": plots})
 
-def make_custom_plot(dataframe, filename, name):
+def first_calc(dataframe, filename, name):
+    x_orig = pd.to_datetime(dataframe['date'])
+    x=np.array(pd.to_datetime(dataframe['date']).index.values, dtype=float)
+    # y_orig = dataframe[name + '_value']
+    y_orig = dataframe[name]
+    y_orig = y_orig
+    y_mean = np.mean(y_orig)
+    y = np.array(y_orig)
+    # y = y
+    stats = linregress(x, y)
+    m = stats.slope
+    b = stats.intercept
+    regres_y = m * x + b
+    result_row = []
+    for index, value in enumerate(regres_y):
+        res_value = y[index] - value
+        result_row.append(res_value)
+    min_sigma = 99999999999
+    min_value_shift = 0
+    sinus_result = []
+
+    for day in range(365):
+        sinus_row = []
+        
+        for index, value in enumerate(result_row):
+            sinus = math.sin(2 * math.pi / 365.25 * index + day)
+            sinus_row.append(sinus)
+        sinus_row = np.array(sinus_row, dtype=float)
+        sinus_regr = linregress(sinus_row, result_row)
+        sinus_m = sinus_regr.slope
+        sinus_b = sinus_regr.intercept
+        sinus_regr_result = sinus_m * sinus_row + sinus_b
+        sig_sum = 0
+        for index, value in enumerate(result_row):
+            sig_sum += (sinus_regr_result[index] - result_row[index])**2
+        sigma = math.sqrt(sig_sum/len(result_row))
+        if (sigma < min_sigma):
+            min_sigma = sigma
+            min_value_shift = day
+            sinus_result = sinus_regr_result
+        
+    y_sin = np.array(y) - np.array(sinus_result)
+    final_regr = linregress(x, y_sin)
+    final_m = final_regr.slope
+    final_stderr = round(final_regr.stderr * 1000 * 365.25, 2)
+    return final_m, final_stderr
+
+
+def make_custom_plot(dataframe, filename, name, rotated, err):
     x_orig = pd.to_datetime(dataframe['date'])
     x=np.array(pd.to_datetime(dataframe['date']).index.values, dtype=float)
     # y_orig = dataframe[name + '_value']
@@ -90,6 +138,8 @@ def make_custom_plot(dataframe, filename, name):
     final_intercept_stderr = final_regr.intercept_stderr
 
     speed_mm_per_year = final_m * 1000 * 365.25
+    rotated_speed = rotated * 1000 * 365.25
+    print(filename, name, speed_mm_per_year, final_stderr, rotated_speed, err)
     sinus_result = sinus_result + y_mean
     
     fig, ax = plt.subplots(figsize=(10, 10))
@@ -98,7 +148,7 @@ def make_custom_plot(dataframe, filename, name):
     ax.plot(x, final_m * x + final_b, color="red")
     ax.get_yaxis().set_major_formatter(
         matplotlib.ticker.FuncFormatter(lambda x, p: format(round(float(x), 4), ',')))
-    # ax.xaxis.set_major_formatter(myFmt)
+    ax.xaxis.set_major_formatter(myFmt)
     ax.set(xlabel='day', ylabel='distance', title= filename + ' ' + name + ' movement time series, speed = ' + str(round(speed_mm_per_year, 2)) + ' mm per year, +-stderr = ' + str(final_stderr))
     plt.setp(ax.get_xticklabels(), rotation=45)
     plt.savefig(path + 'static/plots/final_regr' + '_' + filename + '_' + name + ".png")
@@ -113,13 +163,18 @@ def make_custom():
             angle = tfr_tt_rot_az
         else:
             angle = tfr_ak_rot_az
-        for index in dataframe.index:
-            dataframe.loc[index, 'east_value'] = dataframe.loc[index, 'east_value'] * math.cos(math.radians(angle)) + dataframe.loc[index, 'north_value'] * math.sin(math.radians(angle))
-            dataframe.loc[index, 'north_value'] = - dataframe.loc[index, 'east_value'] * math.sin(math.radians(angle)) + dataframe.loc[index, 'north_value'] * math.cos(math.radians(angle))
-        dataframe.columns = ['date', 'lateral', 'longitudinal']
+        # for index in dataframe.index:
+        #     dataframe.loc[index, 'east_value'] = dataframe.loc[index, 'east_value'] * math.cos(math.radians(angle)) + dataframe.loc[index, 'north_value'] * math.sin(math.radians(angle))
+        #     dataframe.loc[index, 'north_value'] = - dataframe.loc[index, 'east_value'] * math.sin(math.radians(angle)) + dataframe.loc[index, 'north_value'] * math.cos(math.radians(angle))
+        # dataframe.columns = ['date', 'lateral', 'longitudinal']
         # rotate(dataframe, filename)
-        make_custom_plot(dataframe, filename, 'longitudinal')
-        make_custom_plot(dataframe, filename, 'lateral')
+        x, x_err = first_calc(dataframe, filename, 'east_value')
+        y, y_err = first_calc(dataframe, filename, 'north_value')
+        rotated_x = x * math.cos(math.radians(angle)) + y * math.sin(math.radians(angle))
+        rotated_y = -x * math.sin(math.radians(angle)) + y * math.cos(math.radians(angle))
+        dataframe.columns = ['date', 'lateral', 'longitudinal']
+        make_custom_plot(dataframe, filename, 'longitudinal', rotated_x, x_err)
+        make_custom_plot(dataframe, filename, 'lateral', rotated_y, y_err)
 
 
     # xx = x * math.cos(radians) + y * math.sin(radians)
